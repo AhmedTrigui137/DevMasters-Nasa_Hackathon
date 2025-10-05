c8f05279e297cb7ae46d30286095bf760f8953cc
SahnounHoussem <sahnoun.houssem05@gmail.com>
2025-10-05 13:58:20 +0100

    fix build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,17 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'three-vendor';
+            if (id.includes('react') || id.includes('react-dom') || id.includes('@react-three')) return 'react-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
